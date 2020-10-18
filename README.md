adffb863d0cd218f6449b2edd5c367b3a1a95156
Corey Samuels <coreyjsamuels@gmail.com>
2020-10-18 23:27:18 +0100

    Commenting on files and removing Pseudo code

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Here I have created a simple daily planner that can be used regularly to save re
 
 This project was initially a challenge for me as I had to use my limited knowledge of Jquery and Javascript and then introduce new libraries such as moment.js and use them in collaboration with each other to build this planner. This meant that I had to work hard to ensure I was using these right and thoroughly research the documentations to code these libraries into my work. I managed to use them correctly and build a fully functioning planner and so I have taken another step in mastering Javascript!
 
+Use the link https://coreyjay98.github.io/Daily-Planner/ to view the Page!
+
 ### Credits
 
 - Moment.Js
@@ -22,7 +24,7 @@ This project was initially a challenge for me as I had to use my limited knowled
 
 This is a screenshot of the daily planner as of right now (Dates are all in the past as of 10PM)
 
-![Planner Initial] (/Assets/SchedulerPast.PNG)
+![Planner Initial] (Assets/SchedulerPast.PNG)
 
 This is a screenshot of an example of what the planner would look like at a normal time in the working day
-![Colour Example] (/Assets/PlannerExample.PNG)
+![Colour Example] (Assets/PlannerExample.PNG)
--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 const textDivs = $(".textDivs");
 const saveButton = $(".saveBtn");
 const container = $(".container");
-const inputForm = $(".inputForm");
 const rows = $(".row");
 
 var datetime = 0,
   date = 0;
 
+// Function to create Date and run at the top of page
 var update = function () {
   let date = moment();
   datetime.html(date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 };
+//Updates date every second to keep in time with clock
 
 $("document").ready(function () {
   datetime = $("#currentDay");
   update();
   setInterval(update, 1000);
 });
-
+// number value of current moment in 24 hour time
 const currentHour = parseInt(moment().format("HH:mm"));
 
+// Changes each colour of text areas corresponding with time
 $(".textDivs").each(function () {
   let divData = parseInt($(this).attr("id"));
-  console.log("divdata", divData);
-  console.log("currenthour", currentHour);
   if (currentHour > divData) {
-    console.log("past");
     $(this).addClass("past");
   } else if (currentHour === divData) {
-    console.log("present");
     $(this).addClass("present").removeClass("past");
   } else {
-    console.log("future");
     $(this).addClass("future").removeClass("past present");
   }
 });
 
+//Stores any text entered into text boxes
 container.on("click", "button", function (event) {
   event.preventDefault();
   const buttonData = $(this).attr("data-btn");
-  console.log(buttonData);
   let textData = $("input[data-text = '" + buttonData + "']").val();
   localStorage.setItem(buttonData, textData);
 });
 
+//retrieves saved text Items from storage and displays on page
 $(function () {
   $("input[data-text]").each(function () {
     const textKey = $(this).attr("data-text");
